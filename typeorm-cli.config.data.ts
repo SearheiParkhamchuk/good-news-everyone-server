4e0ad172782e1f7c68790ad72bfcdc61a042efa5
Siarhei Parkhamchuk <s.parkhamchuk@gmail.com>
2024-05-25 20:31:52 +0200

    IMPROVEMENT: Divide migrations into two sources

--- typeorm-cli.config.data.ts
+++ typeorm-cli.config.data.ts
@@ -5,12 +5,10 @@ import { NestFactory } from '@nestjs/core/nest-factory';
 import { DataSource } from 'typeorm';
 import { PostgresDbConfig, postgres_db_config } from './src/01-app/typeorm-root/postgres_db.config';
 import { ArticleRepositoryEntity } from './src/04-entities/articles-repository/articles-repository.entity';
-import { CreateArticlesTable1716558248888 } from './src/migrations/articles/1716558248888-create-articles-table';
-import { CreateTypeKeyIndex1716563826609 } from './src/migrations/news-categories/1716563826609-create-type-key-index';
-import { CreateNewsCategoriesTable1716563709169 } from './src/migrations/news-categories/1716563709169-create-news-categories-table';
+import { InsertInitialCategories1716583258194 } from './src/migrations/news-categories/1716583258194-insert-initial-categories';
 import { NewsCategoriesRepositoryEntity } from './src/04-entities/news-categories-repository/news-categories-repository.entity';
 import { NewsSourcesRepositoryEntity } from './src/04-entities/news-sources-repository/news-sources-repository.entity';
-import { CreateNewsSourcesTable1716638750328 } from './src/migrations/news-sources/1716638750328-create-news-sources-table';
+import { InsertInitialNewsSources1716647408625 } from './src/migrations/news-sources/1716647408625-insert-initial-news-sources';
 
 const dataSource = NestFactory.create(ConfigModule.forRoot({ load: [postgres_db_config] })).then(
   (app: INestApplication) => {
@@ -23,13 +21,8 @@ const dataSource = NestFactory.create(ConfigModule.forRoot({ load: [postgres_db_
       password: config.password,
       database: config.database,
       entities: [ArticleRepositoryEntity, NewsCategoriesRepositoryEntity, NewsSourcesRepositoryEntity],
-      migrationsTableName: 'migrations',
-      migrations: [
-        CreateArticlesTable1716558248888,
-        CreateNewsCategoriesTable1716563709169,
-        CreateTypeKeyIndex1716563826609,
-        CreateNewsSourcesTable1716638750328,
-      ],
+      migrations: [InsertInitialCategories1716583258194, InsertInitialNewsSources1716647408625],
+      migrationsTableName: 'migrations-data',
     });
   },
 );
