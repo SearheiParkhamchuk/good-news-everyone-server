eaa746359a20909e699ca9ff79d9b4bc4f0a0e8d
Siarhei Parkhamchuk <s.parkhamchuk@gmail.com>
2024-05-27 22:06:26 +0200

    FIX: Fixed Docker configs. Fixed log message colors

--- src/05-shared/utils/colored-log-message.ts
+++ src/05-shared/utils/colored-log-message.ts
@@ -3,9 +3,9 @@ import { never } from './never';
 export function coloredLogMessage(message: unknown, type: 'error' | 'success' = 'success'): string {
   switch (type) {
     case 'success':
-      return `\x1b[42m${message}\x1b[0m`;
+      return `\x1b[36m${message}\x1b[0m`;
     case 'error':
-      return `\x1b[41m${message}\x1b[0m`;
+      return `\x1b[31m${message}\x1b[0m`;
     default:
       never(null, `Invalid log type ${type}`);
   }
