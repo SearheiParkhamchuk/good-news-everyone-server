ff98e3e3eee643be774bae6e0acc8a8038f291bd
Siarhei Parkhamchuk <s.parkhamchuk@gmail.com>
2024-05-31 14:52:36 +0200

    FIX: Set `forceCloseConnections` to `true` because when app is restarting the app hangs up.

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { ColoredLogger } from './05-shared/modules/logger';
 
 async function bootstrap() {
   const logger = new ColoredLogger();
-  const app = await NestFactory.create(AppModule, { cors: { origin: process.env.APP_ORIGINS.split(',') } });
+  const app = await NestFactory.create(AppModule, {
+    cors: { origin: process.env.APP_ORIGINS.split(',') },
+    forceCloseConnections: true,
+  });
   await app.listen(process.env.APP_PORT, () => {
     logger.log(`App is running on port: ${process.env.APP_PORT}`);
   });
