20e0bf3d88a7e2d33a304c6d5c20ed2a6c4968aa
Siarhei Parkhamchuk <s.parkhamchuk@gmail.com>
2024-06-16 00:48:25 +0200

    FIX[features][news-articles-remote]: Fixed data selection.

--- src/03-features/news-articles-remote/news-articles-remote.service.ts
+++ src/03-features/news-articles-remote/news-articles-remote.service.ts
@@ -18,9 +18,11 @@ export class NewsArticlesRemoteService {
     const raw_sources = await this.news_sources_repository.getAll();
     const sources = raw_sources.map((s) => ({ url: s.url, source: s.source, metadata: s }));
     const raw_response = await this.news_receiver.fetchSources<Metadata>({ sources });
-    const response_with_metadata = raw_response
-      .map((r) => r.data.map((d) => ({ ...d, source: r.metadata })))
-      .flat();
-    return { data: response_with_metadata, errors: NewsSourcesReceiverService.selectErrors(raw_response) };
+
+    const errors = NewsSourcesReceiverService.selectErrors(raw_response);
+    const data = NewsSourcesReceiverService.selectData<Metadata>(raw_response);
+
+    const response_with_metadata = data.map((r) => r.data.map((d) => ({ ...d, source: r.metadata }))).flat();
+    return { data: response_with_metadata, errors };
   }
 }
